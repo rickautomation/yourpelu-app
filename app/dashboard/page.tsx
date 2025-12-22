33389dd7e1389792981625ac40a89b5aebe373a9
Marcelo-Grando <marcelogrando017@gmail.com>
2025-12-22 04:11:55 -0300

    feat(dashboard): add welcome guide with user greeting and step-by-step instructions
    - Display personalized welcome message with user name
    - Added instructional steps for navigating to Barbería, Servicios, and Estilos
    - Structured dashboard layout with clear headings and spacing
    - Prepared dashboard as initial hub for barber shop management

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,32 +1,33 @@
-// app/dashboard/page.tsx
 "use client";
 import { useFakeAuth } from "../lib/useFakeAuth";
+import BarbershopPage from "./barberia/page";
 
 export default function DashboardPage() {
-  const { user, loading, isUnauthorized, router, isAuthenticated } = useFakeAuth();
+  const { user, loading, isUnauthorized, router } = useFakeAuth();
 
-  // if (loading) return <p>Cargando...</p>;
-  // if (isUnauthorized) {
-  //   router.push("/login");
-  //   return null;
-  // }
-
-  console.log("user", user)
+  if (loading) return <p className="text-white">Cargando...</p>;
+  if (isUnauthorized) {
+    router.push("/login");
+    return null;
+  }
 
   return (
-    <div className="flex flex-col space-y-2">
-     <div className="flex text-center items-center p-6 bg-gray-800 rounded-lg shadow-md">
-        <p className="text-2xl">Agregar corte</p>
-        <button className="ml-auto bg-pink-400 text-white px-4 py-2 rounded hover:bg-pink-500 transition-colors">
-          +
-        </button>
-      </div>
-      <div className="flex text-center items-center p-6 bg-gray-800 rounded-lg shadow-md">
-        <p className="text-2xl">Agregar coloracion</p>
-        <button className="ml-auto bg-pink-400 text-white px-4 py-2 rounded hover:bg-pink-500 transition-colors">
-          +
-        </button>
-      </div>
+    <div className="flex flex-col space-y-4 p-2">
+      <h3 className="text-white text-2xl font-semibold mb-4">
+        ¡Bienvenido, {user?.name}!
+      </h3>
+      <h4 className="text-white text-lg mb-4">
+        1. Desde el menu lateral, accede a "Barbería" para gestionar los detalles de tu negocio.
+      </h4>
+      <h4 className="text-white text-lg mb-4">
+        2. Luego, dirígete a "Servicios" para añadir y personalizar los servicios que ofreces.
+      </h4>
+      <h4 className="text-white text-lg mb-4">
+        3. Agrega tus estilos de corte propios en la sección "Estilos".
+      </h4>
+      <h4 className="text-white text-lg mb-4">
+        4. Ya estás listo para empezar a gestionar tu barbería de manera eficiente. ¡Vamos allá!
+      </h4>
     </div>
   );
 }
